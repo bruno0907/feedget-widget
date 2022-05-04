f5671f7e955b3d81e2e084b4e562114061b70c35
Bruno Mariani <bruno0907@gmail.com>
2022-05-03 23:29:00 -0300

    end of day 2

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,13 +1,24 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: ["./src/**/*.tsx"],
   theme: {
     extend: {
       colors: {
         brand: {
+          300: '#996DFF',
           500: '#8257e6'
-        }
+        },
+      },
+      borderRadius: {
+        md: '4px'
+      },
+      fontFamily: {
+        'sans': ['Inter', ...defaultTheme.fontFamily.sans]
       }
     },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),    
+  ],
 }
